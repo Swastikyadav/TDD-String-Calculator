93d6cf2ffae4e988a2a86af2b17e2fad37ca4d4b
Swastik Yadav <swastikb380@gmail.com>
2025-01-12 10:45:47 +0530

    make failing test for new line pass

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function add(str) {
   if (!str) return 0;
+  if (str.length === 1) return +str;
 
-  return str.split(",").reduce((acc, cv) => acc + Number(cv), 0);
+  const newLineSplit = str.split("\n");
+  const newLineLeft = newLineSplit[0] || "";
+  const newLineRight = newLineSplit[1] || "";
+
+  return [...newLineLeft.split(","), ...newLineRight.split(",")].reduce(
+    (acc, cv) => acc + Number(cv),
+    0
+  );
 }
 
 module.exports = {

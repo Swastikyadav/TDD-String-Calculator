9e08780afb8859a7c9e2a01b4a0d0c6dbccac709
Swastik Yadav <swastikb380@gmail.com>
2025-01-12 10:18:42 +0530

    refactor add function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function add(str) {
   if (!str) return 0;
 
-  return str.split(",").reduce((acc, cv) => acc + +cv, 0);
+  return str.split(",").reduce((acc, cv) => acc + Number(cv), 0);
 }
 
 module.exports = {
